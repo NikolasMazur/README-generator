287e7ac158ff960cae83789074b1395eabed2a7e
Nikolas <kolemazur@gmail.com>
2022-04-22 21:59:35 -0600

    Add remaining prompts

--- index.js
+++ index.js
@@ -10,6 +10,56 @@ const questions = () => {
             type: "input",
             name: "title",
             message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter application description."
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please describe the installation process.",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please describe instructions and examples for use.",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Please enter tests that a user can try to test the application with.",
+        },
+        {
+            type: "input",
+            name: "credits",
+            message: "Please list project collaborators.",
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            choices: [
+                "MIT",
+                "APACHE (2.0)",
+                "GPL (3.0)",
+                "BSD (3)",
+                "MPL (2.0)",
+                "CDDL (1.0)",
+                "EPL (2.0)",
+                "None"
+            ],
+            message: "Please select a license.",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter GitHub username.",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter an email for users to reach you at.",
         }
     ])
 };
